f21479ad91e98cc865b6bc171eb7e66c95cfaaeb
rashmika0834 <64023498+rashmika0834@users.noreply.github.com>
2023-08-11 01:28:33 +0530

    made the webpage responsive

--- script.js
+++ script.js
@@ -26,3 +26,23 @@ window.scroll = () => {
     menuIcon.classList.remove('fa-xmark');
     navbar.classList.remove('active');
 };
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('Header navbar a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset +height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header navbar a [href*=' + id +']').classList.add('active');
+            });
+        };
+
+    });
+};
